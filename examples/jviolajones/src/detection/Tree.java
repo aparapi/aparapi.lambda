ed80a7089a13500767e0a7dbe5cc815fb5a330a0
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2012-05-21 13:05:20 +0000

    First mutation converts 2d arrays to 1d arrays.

--- examples/jviolajones/src/detection/Tree.java
+++ examples/jviolajones/src/detection/Tree.java
@@ -3,56 +3,54 @@ package detection;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Tree {
-final static int LEFT = 0;
-final static int RIGHT=1;
+public class Tree{
+   final static int LEFT = 0;
 
-List<Feature> features;
+   final static int RIGHT = 1;
 
-public Tree()
-{
-	features = new ArrayList<Feature>();
-}
-public void addFeature(Feature f)
-{
-	features.add(f);
-}
+   List<Feature> features;
 
-public float getVal(int[][] grayImage, int[][] squares, int i, int j, float scale) {
-	Feature cur_node = features.get(0);
-	while(true)
-	{
-		int where = cur_node.getLeftOrRight(grayImage, squares, i, j, scale);
-		if(where==LEFT)
-		{
-			if(cur_node.has_left_val)
-			{
-				//System.out.println("LEFT");
-				return cur_node.left_val;
-			}
-			else
-			{
-				//System.out.println("REDIRECTION !");
-				//System.exit(0);
-				cur_node = features.get(cur_node.left_node);
-			}
-		}
-		else
-		{
-			if(cur_node.has_right_val)
-			{
+   public Tree() {
 
-				//System.out.println("RIGHT");
-				return cur_node.right_val;
-			}
-			else
-			{
-				//System.out.println("REDIRECTION !");
-				//System.exit(0);
-				cur_node = features.get(cur_node.right_node);
-			}
-		}
-	}
-}
+      features = new ArrayList<Feature>();
+
+   }
+
+   public void addFeature(Feature f) {
+
+      features.add(f);
+
+   }
+
+   public float getVal(int[] grayImage, int[] squares, int width, int height,int i, int j, float scale) {
+
+      Feature cur_node = features.get(0);
+      while (true) {
+         int where = cur_node.getLeftOrRight(grayImage, squares, width, height, i, j, scale);
+         if (where == LEFT) {
+            if (cur_node.has_left_val) {
+               //System.out.println("LEFT");
+
+               return cur_node.left_val;
+            } else {
+              // System.out.println("REDIRECTION !");
+               //System.exit(0);
+               cur_node = features.get(cur_node.left_node);
+            }
+         } else {
+            if (cur_node.has_right_val) {
+
+             //  System.out.println("RIGHT");
+
+               return cur_node.right_val;
+            } else {
+             //  System.out.println("REDIRECTION !");
+               //System.exit(0);
+               cur_node = features.get(cur_node.right_node);
+            }
+         }
+      }
+
+   }
 
 }
