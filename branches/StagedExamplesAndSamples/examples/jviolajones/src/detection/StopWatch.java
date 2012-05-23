622c64f3bcf57fec161b3c68bfff8e1daee6e36b
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2012-05-23 22:47:10 +0000

    Multi-threaded the simple (non-canny path) which reduces face detection from 4000 +ms to 1400ms on my 4 core Llano.

--- branches/StagedExamplesAndSamples/examples/jviolajones/src/detection/StopWatch.java
+++ branches/StagedExamplesAndSamples/examples/jviolajones/src/detection/StopWatch.java
@@ -16,19 +16,34 @@ as the basis of an Aparapi example.
 **/
 
 public class StopWatch{
+   String message = "timer";
+
    long start = 0L;
 
+   public StopWatch(String _message) {
+      message = _message;
+      start();
+   }
+
+   public StopWatch() {
+      this("timer");
+   }
+
    public void start() {
-
       start = System.nanoTime();
+   }
 
+   public void stop() {
+      print(message);
+   }
+
+   private long end() {
+      return ((System.nanoTime() - start) / 1000000);
    }
 
    public void print(String _str) {
-
-      long end = (System.nanoTime() - start) / 1000000;
-      System.out.println(_str + " " + end);
-
+      System.out.println(_str + " " + end());
+      start();
    }
 
 }
