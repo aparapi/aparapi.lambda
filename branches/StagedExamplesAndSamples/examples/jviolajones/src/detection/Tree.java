6a1cb9ac660f13323617be77126ca516e3d5f964
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2012-05-24 16:16:15 +0000

    Remove haarcascade methods from stage, feature, tree etc

--- branches/StagedExamplesAndSamples/examples/jviolajones/src/detection/Tree.java
+++ branches/StagedExamplesAndSamples/examples/jviolajones/src/detection/Tree.java
@@ -57,40 +57,4 @@ public class Tree{
       }
    }
 
-   public float getVal(int[] grayImage, int[] squares, int width, int height, int i, int j, float scale) {
-
-      // System.out.println("stage id "+stage.id+" tree id"+id);
-      Feature cur_node = features.get(0);
-
-      while (true) {
-         //  System.out.println("feature id "+cur_node.id);
-         int where = cur_node.getLeftOrRight(grayImage, squares, width, height, i, j, scale);
-         if (where == LEFT) {
-            if (cur_node.has_left_val) {
-               //  System.out.println("left-val");
-
-               return cur_node.left_val;
-            } else {
-               // System.out.println("REDIRECTION !");
-               //System.exit(0);
-               //  System.out.println("left");
-               cur_node = features.get(cur_node.left_node);
-            }
-         } else {
-            if (cur_node.has_right_val) {
-               //  System.out.println("right-val");
-               //  System.out.println("RIGHT");
-
-               return cur_node.right_val;
-            } else {
-               //  System.out.println("REDIRECTION !");
-               //System.exit(0);
-               cur_node = features.get(cur_node.right_node);
-               // System.out.println("right");
-            }
-         }
-      }
-
-   }
-
 }
