6b0a5b2db87fffbba28da719b6ff70eb12466e3f
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2012-12-02 22:57:24 +0000

    experiments with MockLambda

--- branches/MockLambda/run.bash
+++ branches/MockLambda/run.bash
@@ -1,4 +1,11 @@
-. ./setvars.bash
-${JAVA_HOME}/bin/java \
+/Users/garyfrost/lambda/build/macosx-x86_64-fastdebug/bin/java \
+    -XX:+UnlockDiagnosticVMOptions \
+    -XX:+LogCompilation  \
+    -XX:+PrintOptoAssembly  \
+    -XX:+PrintLIR \
     -classpath aparapi.jar:samples.jar \
     com.amd.aparapi.samples.$1 $2 $3 $4 $6 $7 $8 $9 
+
+#    -XX:+PrintLIR 
+#    -XX:+LogCompilation 
+#    -XX:+PrintOptoAssembly 
