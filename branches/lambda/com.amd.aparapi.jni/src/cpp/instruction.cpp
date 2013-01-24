320983056cc3394dfc5cc6afdf999202b6842f67
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-01-24 21:46:35 +0000

    More instruction decoding...

--- branches/lambda/com.amd.aparapi.jni/src/cpp/instruction.cpp
+++ branches/lambda/com.amd.aparapi.jni/src/cpp/instruction.cpp
@@ -3,6 +3,67 @@
 
 #include <string.h>
 
+Instruction::Instruction(ByteBuffer *_codeByteBuffer){
+   int pc = _codeByteBuffer->getOffset();
+   byte_t byte= _codeByteBuffer->u1();
+   ByteCode byteCode = bytecode[byte];
+   fprintf(stderr, "%d %s", pc, (char*)byteCode.name);
+   switch(byteCode.immSpec){
+      case ImmSpec_NONE:
+         break;
+      case ImmSpec_Blvti:
+         immSpec_Blvti.lvti=_codeByteBuffer->u1();
+         break;
+      case ImmSpec_Bcpci:
+         immSpec_Bcpci.cpci=_codeByteBuffer->u1();
+         break;
+      case ImmSpec_Scpci:
+         immSpec_Scpci.cpci= _codeByteBuffer->u2();
+         break;
+      case ImmSpec_Bconst:
+         immSpec_Bconst.value= _codeByteBuffer->u1();
+         break;
+      case ImmSpec_Sconst:
+         immSpec_Sconst.value= _codeByteBuffer->u2();
+         break;
+      case ImmSpec_IorForS:
+         immSpec_IorForS.i= _codeByteBuffer->u4();
+         break;
+      case ImmSpec_Spc:
+         immSpec_Spc.pc= _codeByteBuffer->s2() +pc;
+         fprintf(stderr, " %d", immSpec_Spc.pc);
+         break;
+      case ImmSpec_Scpfi:
+         immSpec_Scpfi.cpfi= _codeByteBuffer->u2();
+         break;
+      case ImmSpec_ScpmiBB:
+         immSpec_ScpmiBB.cpmi= _codeByteBuffer->u2();
+         immSpec_ScpmiBB.b1= _codeByteBuffer->u1();
+         immSpec_ScpmiBB.b2= _codeByteBuffer->u1();
+         break;
+      case ImmSpec_BlvtiBconst:
+         immSpec_BlvtiBconst.lvti= _codeByteBuffer->u1();
+         immSpec_BlvtiBconst.value= _codeByteBuffer->u1();
+         break;
+      case ImmSpec_Scpmi:
+         immSpec_Scpmi.cpmi= _codeByteBuffer->u2();
+         break;
+      case ImmSpec_ScpciBdim:
+         immSpec_ScpciBdim.cpci= _codeByteBuffer->u2();
+         immSpec_ScpciBdim.dim= _codeByteBuffer->u1();
+         break;
+      case ImmSpec_Ipc:
+         immSpec_Ipc.pc= _codeByteBuffer->s4()+pc;
+         fprintf(stderr, " %d", immSpec_Ipc.pc);
+         break;
+      case ImmSpec_UNKNOWN:
+         break;
+
+   }
+   fprintf(stderr, "\n");
+}
+
+
 void Decoder::list(u1_t* buf, u4_t len){
    fprintf(stdout, "inside list\n");
 }
