6beac086a1c7bac635d85c3e305794d2b6a0fdc6
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-01-25 04:21:43 +0000

    looking for memory leak

--- branches/lambda/com.amd.aparapi.jni/src/cpp/classtoolstest.cpp
+++ branches/lambda/com.amd.aparapi.jni/src/cpp/classtoolstest.cpp
@@ -15,45 +15,46 @@
 #include "instruction.h"
 
 int main(int argc, char **argv){
-   FILE *classFile = fopen("Main.class", "rb");
-   if (classFile == NULL){fputs ("OPen error",stderr); exit (1);}
-   fseek(classFile, 0 , SEEK_END);
-   long size = ftell(classFile);
-   rewind (classFile);
+  FILE *classFile = fopen("Main.class", "rb");
+  if (classFile == NULL){fputs ("OPen error",stderr); exit (1);}
+  fseek(classFile, 0 , SEEK_END);
+  long size = ftell(classFile);
+  rewind (classFile);
 
-   // allocate memory to contain the whole file:
-   char *buffer = (char*) malloc (sizeof(char)*size);
-   if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
+  // allocate memory to contain the whole file:
+  char *buffer = (char*) malloc (sizeof(char)*size);
+  if (buffer == NULL) {fputs ("Memory error",stderr); exit (2);}
 
-   // copy the file into the buffer:
-   size_t result = fread (buffer,1,size, classFile);
-   fclose (classFile);
-   if (result != size) {fputs ("Reading error",stderr); exit (3);}
+  // copy the file into the buffer:
+  size_t result = fread (buffer,1,size, classFile);
+  fclose (classFile);
+  if (result != size) {fputs ("Reading error",stderr); exit (3);}
 
-   fprintf(stdout, "read %ld bytes\n", size);
+  fprintf(stdout, "read %ld bytes\n", size);
 
-   ByteBuffer byteBuffer((byte_t*)buffer, size);
+  ByteBuffer *byteBuffer = new ByteBuffer((byte_t*)buffer, size);
 
-   ClassInfo classInfo(&byteBuffer);
-   MethodInfo *methodInfo = classInfo.getMethodInfo((char*)"getCount", (char*)"(FF)I");
-   CodeAttribute *codeAttribute = methodInfo->getCodeAttribute();
-   ByteBuffer *codeByteBuffer = codeAttribute->getCodeByteBuffer();
+  ClassInfo classInfo(byteBuffer);
+  MethodInfo *methodInfo = classInfo.getMethodInfo((char*)"getCount", (char*)"(FF)I");
+  CodeAttribute *codeAttribute = methodInfo->getCodeAttribute();
+  ByteBuffer *codeByteBuffer = codeAttribute->getCodeByteBuffer();
 
-   Instruction** instructions = new Instruction*[codeByteBuffer->getLen()];
-   for (unsigned i=0; i< codeByteBuffer->getLen(); i++){
-      instructions[i] = NULL;
-   }
+  Instruction** instructions = new Instruction*[codeByteBuffer->getLen()];
+  for (unsigned i=0; i< codeByteBuffer->getLen(); i++){
+    instructions[i] = NULL;
+  }
 
-   while (!codeByteBuffer->empty()){
-      Instruction *instruction = new Instruction(codeByteBuffer);
-      instructions[instruction->getPC()] = instruction;
-      instruction->write(stdout, classInfo.getConstantPool());
-   }
+  while (!codeByteBuffer->empty()){
+    Instruction *instruction = new Instruction(codeByteBuffer);
+    instructions[instruction->getPC()] = instruction;
+    instruction->write(stdout, classInfo.getConstantPool());
+  }
 
-   //   delete methodInfo;
+  delete methodInfo;
+  delete byteBuffer;
 
-   /* the whole file is now loaded in the memory buffer. */
+  /* the whole file is now loaded in the memory buffer. */
 
-   // terminate
+  // terminate
 
 }
