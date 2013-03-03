b7bc2d162990401cc3136bc1e30bf66931e0b519
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-03-03 20:55:25 +0000

    mandel2D.sh is linux/mac neutral

--- samples/mandel/mandel2D.sh
+++ samples/mandel/mandel2D.sh
@@ -1,6 +1,10 @@
-#-agentpath:../../com.amd.aparapi.jni/dist/libaparapi_x86_64.dyLib
+LIBPREFIX=../../com.amd.aparapi.jni/dist/libaparapi_$(uname -m)
+case $(uname -s) in 
+  Darwin) LIBNAME=${LIBPREFIX}.dyLib;;
+  Linux)  LIBNAME=${LIBPREFIX}.so;;
+esac
 java\
- -agentpath:../../com.amd.aparapi.jni/dist/libaparapi_x86_64.so\
+ -agentpath:${LIBNAME}\
  -Dcom.amd.aparapi.executionMode=$1\
  -classpath ../../com.amd.aparapi/dist/aparapi.jar:mandel.jar\
  com.amd.aparapi.sample.mandel.Main2D
