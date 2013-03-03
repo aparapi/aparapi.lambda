115d5d0e4e12d16f41d9957c5059c5ba3794a58f
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-03-03 20:57:43 +0000

    bash scripts should be linux/mac neutral and all use jvmti agent

--- samples/life/life-agent.sh
+++
@@ -1,7 +0,0 @@
-#-agentpath:../../com.amd.aparapi.jni/dist/libaparapi_x86_64.so
-java\
- -agentpath:../../com.amd.aparapi.jni/dist/libaparapi_x86_64.dylib\
- -Dcom.amd.aparapi.executionMode=$1\
- -Dcom.amd.aparapi.useAgent=true\
- -classpath ../../com.amd.aparapi/dist/aparapi.jar:life.jar\
- com.amd.aparapi.sample.life.Main
--- samples/life/life.sh
+++ samples/life/life.sh
@@ -1,5 +1,10 @@
+LIBPREFIX=../../com.amd.aparapi.jni/dist/libaparapi_$(uname -m)
+case $(uname -s) in 
+  Darwin) LIBNAME=${LIBPREFIX}.dyLib;;
+  Linux)  LIBNAME=${LIBPREFIX}.so;;
+esac
 java\
- -Djava.library.path=../../com.amd.aparapi.jni/dist\
+ -agentpath:${LIBNAME}\
  -Dcom.amd.aparapi.executionMode=$1\
  -classpath ../../com.amd.aparapi/dist/aparapi.jar:life.jar\
  com.amd.aparapi.sample.life.Main
