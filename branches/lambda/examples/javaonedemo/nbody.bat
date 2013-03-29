807f708d692153d4535ca087f0178d0885a3971c
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-03-29 21:27:37 +0000

    examples needed to use agentpath - oopnbody and blackscholes broken

--- branches/lambda/examples/javaonedemo/nbody.bat
+++ branches/lambda/examples/javaonedemo/nbody.bat
@@ -1,6 +1,7 @@
 @echo off
 java ^
-  -Djava.library.path=..\..\com.amd.aparapi.jni\dist;..\third-party\jogamp\windows-%PROCESSOR_ARCHITECTURE% ^
+  -agentpath:..\..\com.amd.aparapi.jni\dist\aparapi_x86_64.dll ^
+  -Djava.library.path=..\third-party\jogamp\windows-%PROCESSOR_ARCHITECTURE% ^
   -classpath ..\third-party\jogamp\gluegen-rt.jar;..\third-party\jogamp\jogl-all.jar;..\..\com.amd.aparapi\dist\aparapi.jar;javaonedemo.jar ^
   com.amd.aparapi.examples.javaonedemo.NBody 
 
