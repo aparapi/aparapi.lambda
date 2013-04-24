3ff0d25e3747006206fbe44b5169d970e1914bc9
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-04-24 23:06:01 +0000

    Add support for more instructions

--- branches/lambda/com.amd.aparapi/src/java/kerneltest/SquaresBare.java
+++ branches/lambda/com.amd.aparapi/src/java/kerneltest/SquaresBare.java
@@ -4,6 +4,7 @@ import com.amd.aparapi.AparapiException;
 import com.amd.aparapi.ClassModel;
 import com.amd.aparapi.ClassParseException;
 import com.amd.aparapi.Device;
+import com.amd.aparapi.OpenCLDevice;
 
 
 public class SquaresBare{
@@ -12,7 +13,7 @@ public class SquaresBare{
 
 
    void run (int[] in, int[] out, int gid){
-         out[gid] = in[gid] * in[gid];
+         out[gid] = in[gid]++ * in[gid];
       }
 
 
@@ -21,8 +22,5 @@ public class SquaresBare{
       ClassModel classModel = ClassModel.getClassModel(SquaresBare.class);
       ClassModel.ClassModelMethod method = classModel.getMethod("run", "([I[II)V");
       method.getInstructions();
-
-
-
    }
 }
