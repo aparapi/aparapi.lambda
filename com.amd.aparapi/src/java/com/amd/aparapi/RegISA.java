04e2dbed7f656733ab53e8357eeeb61ec462aef9
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-05-25 16:48:03 +0000

    Testing dup, dup2.  Seems to work for simple cases.  Problem optimizing unnecessary movs.

--- com.amd.aparapi/src/java/com/amd/aparapi/RegISA.java
+++ com.amd.aparapi/src/java/com/amd/aparapi/RegISA.java
@@ -951,7 +951,7 @@ public class RegISA{
    List<RegInstruction> instructions = new ArrayList<RegInstruction>();
    ClassModel.ClassModelMethod method;
 
-   boolean optimizeMoves =  true || Config.enableOptimizeRegMoves;
+   boolean optimizeMoves =  false || Config.enableOptimizeRegMoves;
 
    void add(RegInstruction _regInstruction){
       // before we add lets see if this is a redundant mov
--- com.amd.aparapi/src/java/hsailtest/Squares.java
+++ com.amd.aparapi/src/java/hsailtest/Squares.java
@@ -6,13 +6,13 @@ import com.amd.aparapi.*;
 public class Squares{
 
 
-   void run(float[] in, float[] out, int gid){
+   void run(int[] in, int[] out, int gid){
 
       //float f = 9f;
       //for (int i=0; i< 4; i++){
-      //  float f = in[gid];
+       // int f = in[gid];
        //  if (f >3f && gid <7){
-            out[gid] = in[gid] * in[gid];
+            out[gid++] = in[gid] * in[++gid];
        //  }
         // boolean l = in[gid]<10f;
     //  }
@@ -21,30 +21,43 @@ public class Squares{
 
    public static void main(String[] args) throws AparapiException{
       ClassModel classModel = ClassModel.getClassModel(Squares.class);
-      ClassModel.ClassModelMethod method = classModel.getMethod("run", "([F[FI)V");
+      ClassModel.ClassModelMethod method = classModel.getMethod("run", "([I[II)V");
       method.getInstructions();
 
       OkraRunner runner = new OkraRunner();
-      float in[] = new float[10];
-      float out[] = new float[in.length];
+      int in[] = new int[10];
+      int out[] = new int[in.length];
       for (int i=0; i< in.length; i++){
          in[i]=i;
 
-         out[i]=0f;
+         out[i]=0;
       }
       RegISARenderer renderer = new RegISARenderer();
       //renderer.setShowLineNumbers(false);
-      //renderer.setShowComments(true);
+      renderer.setShowComments(true);
       new RegISA(method).render(renderer);
       System.out.println(renderer.toString());
 
       Squares s = new Squares();
-      runner.run(renderer.toString(), in.length, s, in, out, in.length);
+      runner.run(renderer.toString(), in.length-2, s, in, out, in.length-2);
       for (int i=0; i< in.length; i++){
          System.out.print("("+in[i]+","+out[i]+"),");
 
       }
       System.out.println();
+       for (int i=0; i< in.length; i++){
+           in[i]=i;
+
+           out[i]=0;
+       }
+       Squares main = new Squares();
+       for (int i=0; i< in.length-2; i++){
+         main.run(in, out, i);
+       }
+       for (int i=0; i< in.length; i++){
+           System.out.print("("+in[i]+","+out[i]+"),");
+
+       }
          //System.out.println(InstructionHelper.getLabel(i,true, false, false));
 
 
