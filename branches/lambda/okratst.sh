7435c2604429b2aede6a21a2f104257430f518d2
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-06-26 23:09:14 +0000

    synced with wiki doc

--- branches/lambda/okratst.sh
+++ branches/lambda/okratst.sh
@@ -1,4 +1,1 @@
-export OKRA=/home/gfrost/okra/okra/dist
-export PATH=${PATH}:${OKRA}/bin
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${OKRA}/bin
-java -agentpath:com.amd.aparapi.jni/dist/libaparapi_x86_64.so -cp com.amd.aparapi/dist/aparapi.jar:${OKRA}/okra.jar $1
+java -agentpath:com.amd.aparapi.jni/dist/libaparapi_x86_64.so -cp com.amd.aparapi/dist/aparapi.jar:${OKRA}/dist/okra.jar $1
