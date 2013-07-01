badfe64d35a0e974bcec29458239b12456fef59e
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-07-01 14:06:07 +0000

    add -XX:-UseCompressedOops

--- okratst.sh
+++ okratst.sh
@@ -1,1 +1,1 @@
-java -agentpath:com.amd.aparapi.jni/dist/libaparapi_x86_64.so -cp com.amd.aparapi/dist/aparapi.jar:${OKRA}/dist/okra.jar $1
+java -XX:-UseCompressedOops -agentpath:com.amd.aparapi.jni/dist/libaparapi_x86_64.so -cp com.amd.aparapi/dist/aparapi.jar:${OKRA}/dist/okra.jar $1
