b1f6dd4187b9b0a80607de4cc36e016da4f63205
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-07-22 19:34:27 +0000

    add a mock okra lib (mockra) for windows and macos builds

---
+++ branches/lambda/mockra/src/java/com/amd/okra/OkraKernel.java
@@ -0,0 +1,20 @@
+package com.amd.okra;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: gfrost
+ * Date: 7/22/13
+ * Time: 2:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class OkraKernel{
+   public OkraKernel(OkraContext context, String _hsail, String _s){
+   }
+
+   public void setLaunchAttributes(int launchAttributes){
+
+   }
+
+   public void dispatchWithArgs(Object ...args){
+   }
+}
