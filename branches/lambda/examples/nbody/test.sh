c80149be420e9704fe10f60ff58bc5e0aa19fa30
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-07-23 03:35:38 +0000

    hacked multi particles to emulate 3d particles

--- branches/lambda/examples/nbody/test.sh
+++ branches/lambda/examples/nbody/test.sh
@@ -5,10 +5,8 @@ case $(uname -s) in
 esac
 java\
   -agentpath:${LIBNAME}\
-  -Dcom.amd.aparapi.executionMode=$1 \
-  -Dbodies=$2 \
-  -Dheight=600 \
-  -Dwidth=600 \
+  -Dheight=1000 \
+  -Dwidth=1000 \
   -classpath ../third-party/jogamp/jogl-all.jar:../third-party/jogamp/gluegen-rt.jar:../../com.amd.aparapi/dist/aparapi.jar:nbody.jar \
   com.amd.aparapi.examples.nbody.Test
 
