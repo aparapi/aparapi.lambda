e1a1a7e2c2a9821a629a47de0b299861aef5a7db
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-08-09 15:08:02 +0000

    can now cache kernels  - mandel 60fps

--- com.amd.aparapi/src/java/com/amd/aparapi/HSADevice.java
+++ com.amd.aparapi/src/java/com/amd/aparapi/HSADevice.java
@@ -2,95 +2,81 @@ package com.amd.aparapi;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.concurrent.BrokenBarrierException;
-import java.util.concurrent.CyclicBarrier;
 import java.util.function.IntConsumer;
 
-public class HSADevice extends Device{
+public class HSADevice extends Device {
     // TODO: We need to cache somewhere to avoid creating HSAIl each time
-   static Map<IntConsumer, String> map = new HashMap<IntConsumer, String>();
-    static Map<IntConsumer, Object[]> mapArg = new HashMap<IntConsumer, Object[]>();
-    static Map<IntConsumer, OkraRunner> mapRunner = new HashMap<IntConsumer, OkraRunner>();
-   public Device forEach(int size, IntConsumer ic){
-      try{
-          String source = null;
-          Object[] args;
-          OkraRunner runner;
-         if (map.containsKey( ic)){
-            System.out.println("seen!");
-            source = map.get(ic);
-             args = mapArg.get(ic);
-             runner = mapRunner.get(ic);
-          }else{
-         LambdaKernelCall lkc = new LambdaKernelCall(ic);
-         // System.out.println("lambda method " + lkc.getLambdaMethodName());
-         //  System.out.println("lambda method sig " + lkc.getLambdaMethodSignature());
-         // System.out.println("lambda method class "+lkc.getLambdaKernelClass());
-         ClassModel classModel = ClassModel.getClassModel(lkc.getLambdaKernelClass());
-         ClassModel.ClassModelMethod method = classModel.getMethod(lkc.getLambdaMethodName(), lkc.getLambdaMethodSignature());
+    static class CachedRunner {
+        String hsail;
+        Object instance;
 
-         runner = new OkraRunner();
-         HSAILRenderer renderer = new HSAILRenderer().setShowComments(true);
-         HSAILMethod.getHSAILMethod(method, null).renderEntryPoint(renderer);
-         System.out.println(renderer.toString());
+        OkraRunner runner;
+        boolean isStatic;
+        Field[] capturedFields;
+        Object[] args;
+    }
 
-         List<Object> argList = new ArrayList<Object>();
-         if (!lkc.isStatic()){
-            argList.add(lkc.getLambdaKernelThis());
-         }
-             try {
-         for(Field f : lkc.getLambdaCapturedFields()){
-            f.setAccessible(true);
-            String name = f.getName();
-            Type type = f.getType();
-            if (type.equals(float.class)){
+    static Map<Class<? extends IntConsumer>, CachedRunner> map = new HashMap<Class<? extends IntConsumer>, CachedRunner>();
 
-                    argList.add(f.getFloat(ic));
-
-            } else
-             if (type.equals(int.class)){
-
-                 argList.add(f.getInt(ic));
-
-             } else {
-            // if (type.equals(Object.class)){
-
-                 argList.add(f.get(ic));
-
-             }
-
-             System.out.println("name " + name);
-
-         }
-             } catch (IllegalAccessException e) {
-                 e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-             }
-         argList.add(0); // for the gid!
-         source = renderer.toString();
-             args =   argList.toArray(new Object[0]);
-             for (Object o:args){
-                 System.out.println(o+" "+o.getClass().isPrimitive());
-             }
-         map.put(ic, source);
-             mapArg.put(ic, args );
-             mapRunner.put(ic, runner);
-         }
-
-         runner.run(source, size, args);
+    public Device forEach(int size, IntConsumer ic) {
+        try {
+            CachedRunner cachedRunner = null;
+            if (map.containsKey(ic.getClass())) {
+                cachedRunner = map.get(ic.getClass());
+            } else {
+                cachedRunner = new CachedRunner();
+                LambdaKernelCall lkc = new LambdaKernelCall(ic);
+                ClassModel classModel = ClassModel.getClassModel(lkc.getLambdaKernelClass());
+                ClassModel.ClassModelMethod method = classModel.getMethod(lkc.getLambdaMethodName(), lkc.getLambdaMethodSignature());
 
 
-      }catch(AparapiException e){
-         e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-      }catch(ClassNotFoundException e){
-         e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-      }
+                HSAILRenderer renderer = new HSAILRenderer().setShowComments(true);
+                HSAILMethod.getHSAILMethod(method, null).renderEntryPoint(renderer);
+                cachedRunner.hsail = renderer.toString();
+                System.out.println(cachedRunner.hsail);
+                cachedRunner.runner = new OkraRunner(cachedRunner.hsail);
+                cachedRunner.isStatic = lkc.isStatic();
+                if (!cachedRunner.isStatic) {
+                    cachedRunner.instance = lkc.getLambdaKernelThis();
+                }
+                cachedRunner.capturedFields= lkc.getLambdaCapturedFields();
+                cachedRunner.args = new Object[cachedRunner.capturedFields.length+(cachedRunner.isStatic?0:1)+1];
 
 
-      return (this);
+                map.put(ic.getClass(), cachedRunner);
+            }
+            int arg=0;
+            if (!cachedRunner.isStatic){
+                cachedRunner.args[arg++]=cachedRunner.instance;
+            }
+            try {
+                for (Field f : cachedRunner.capturedFields) {
+                    f.setAccessible(true);
+                  //  String name = f.getName();
+                    Type type = f.getType();
+                    if (type.equals(float.class)) {
+                        cachedRunner.args[arg++]= f.getFloat(ic);
+                    } else if (type.equals(int.class)) {
+                        cachedRunner.args[arg++]=f.getInt(ic);
+                    } else {
+                        cachedRunner.args[arg++]=f.get(ic);
+                    }
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+            cachedRunner.args[arg++]=0;
+            cachedRunner.runner.run(size, cachedRunner.args);
 
-   }
+
+        } catch (AparapiException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        return (this);
+
+    }
 }
