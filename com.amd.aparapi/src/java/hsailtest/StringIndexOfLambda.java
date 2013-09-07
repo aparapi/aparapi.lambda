de2c438a6fc005fffddca82e48fd85d866203bf2
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-09-07 21:31:47 +0000

    Handlng of methods with early return is the issue with String.indexOf() early returns must branch to method exit

--- com.amd.aparapi/src/java/hsailtest/StringIndexOfLambda.java
+++ com.amd.aparapi/src/java/hsailtest/StringIndexOfLambda.java
@@ -22,19 +22,62 @@ public class StringIndexOfLambda {
     }
 
 
+    static int indexOf(char[] source, int sourceOffset, int sourceCount,
+                       char[] target, int targetOffset, int targetCount,
+                       int fromIndex) {
+     //   if (fromIndex >= sourceCount) {
+      //      return (targetCount == 0 ? sourceCount : -1);
+       // }
+      //  if (fromIndex < 0) {
+      //      fromIndex = 0;
+       // }
+       // if (targetCount == 0) {
+       //     return fromIndex;
+      //  }
 
+        char first = target[targetOffset];
+        int max = sourceOffset + (sourceCount - targetCount);
+        int found = -1;
+
+        for (int i = sourceOffset + fromIndex; found == -1 && i <= max; i++) {
+            /* Look for first character. */
+            if (source[i] != first) {
+                while (++i <= max && source[i] != first);
+            }
+
+            /* Found first character, now look at the rest of v2 */
+            if (i <= max) {
+                int j = i + 1;
+                int end = j + targetCount - 1;
+                for (int k = targetOffset + 1; j < end && source[j]
+                        == target[k]; j++, k++);
+
+                if (j == end) {
+                    /* Found whole string. */
+                    found =  i - sourceOffset;
+                }
+            }
+        }
+        return found;
+    }
 
     public static void main(String[] args) throws AparapiException {
         String[] strings = new String[]{"cat","mat","dog"};
+        char[][] chars = new char[strings.length][];
+        for (int i=0; i< strings.length;i++){
+            chars[i] = strings[i].toCharArray();
+        }
         int len = strings.length;
         String text = "the cat sat on the mat";
+        char[] textChars = text.toCharArray();
         int[] indices = new int[len];
 
 
 
         IntConsumer ic = gid -> {
-
-            indices[gid] = text.indexOf(strings[gid], 0);
+            indices[gid] = indexOf(textChars, 0, textChars.length,
+                    chars[gid], 0, chars[gid].length, 0);
+           // indices[gid] = text.indexOf(strings[gid], 0);
 
 
 
