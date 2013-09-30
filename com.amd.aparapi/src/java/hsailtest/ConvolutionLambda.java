4a70ecd412cd50c02239c24ebfdc94e0da8fd89c
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-09-30 20:01:02 +0000

    added convolution

--- com.amd.aparapi/src/java/hsailtest/ConvolutionLambda.java
+++ com.amd.aparapi/src/java/hsailtest/ConvolutionLambda.java
@@ -22,11 +22,7 @@ import com.amd.aparapi.Device;
 public class ConvolutionLambda {
 
 
-
-
-
-
-       // private byte imageIn[], imageOut[];
+    // private byte imageIn[], imageOut[];
 
 
     int height;
@@ -45,86 +41,86 @@ public class ConvolutionLambda {
     JLabel imageLabel;
 
     public void processPixel(float _convMatrix3x3[], int x, int y, int w, int h) {
-            float accum = 0f;
-            int count = 0;
-            for (int dx = -3; dx < 6; dx += 3) {
-                for (int dy = -1; dy < 2; dy += 1) {
-                    int rgb = 0xff & inBytes[((y + dy) * w) + (x + dx)];
+        float accum = 0f;
+        int count = 0;
+        for (int dx = -3; dx < 6; dx += 3) {
+            for (int dy = -1; dy < 2; dy += 1) {
+                int rgb = 0xff & inBytes[((y + dy) * w) + (x + dx)];
 
-                    accum += rgb * _convMatrix3x3[count++];
-                }
+                accum += rgb * _convMatrix3x3[count++];
             }
-            byte value = (byte) (Math.max(0, Math.min((int) accum, 255)));
-            outBytes[y * w + x] = value;
-
         }
+        byte value = (byte) (Math.max(0, Math.min((int) accum, 255)));
+        outBytes[y * w + x] = value;
+
+    }
 
 
-        public void applyConvolution(float[] _convMatrix3x3, Device _device) {
-           long start=System.currentTimeMillis();
-            _device.forEach(3 * width * height, i -> {
-                int x = i % (width * 3);
-                int y = i / (width * 3);
+    public void applyConvolution(float[] _convMatrix3x3, Device _device) {
+        long start = System.currentTimeMillis();
+        _device.forEach(3 * width * height, i -> {
+            int x = i % (width * 3);
+            int y = i / (width * 3);
 
-                if (x > 3 && x < (width * 3 - 3) && y > 1 && y < (height - 1)) {
-                    processPixel(_convMatrix3x3, x, y, width * 3, height);
-                }
-            });
-            outputImage.getGraphics().drawString(System.currentTimeMillis()-start+" ms",110,110);
-            imageLabel.repaint();
-        }
+            if (x > 3 && x < (width * 3 - 3) && y > 1 && y < (height - 1)) {
+                processPixel(_convMatrix3x3, x, y, width * 3, height);
+            }
+        });
+        outputImage.getGraphics().drawString(System.currentTimeMillis() - start + " ms", 110, 110);
+        imageLabel.repaint();
+    }
 
-        ConvolutionLambda( File _file) throws IOException {
+    ConvolutionLambda(File _file) throws IOException {
 
-            JFrame frame = new JFrame("Convolution Viewer");
+        JFrame frame = new JFrame("Convolution Viewer");
 
-            inputImage = ImageIO.read(_file);
+        inputImage = ImageIO.read(_file);
 
-            height = inputImage.getHeight();
+        height = inputImage.getHeight();
 
-            width = inputImage.getWidth();
+        width = inputImage.getWidth();
 
-            outputImage = new BufferedImage(width, height, inputImage.getType());
+        outputImage = new BufferedImage(width, height, inputImage.getType());
 
-            gc = outputImage.createGraphics();
+        gc = outputImage.createGraphics();
 
-            inBytes = ((DataBufferByte) inputImage.getRaster().getDataBuffer()).getData();
-            outBytes = ((DataBufferByte) outputImage.getRaster().getDataBuffer()).getData();
+        inBytes = ((DataBufferByte) inputImage.getRaster().getDataBuffer()).getData();
+        outBytes = ((DataBufferByte) outputImage.getRaster().getDataBuffer()).getData();
 
-            imageLabel = new JLabel();
-            imageLabel.setIcon(new ImageIcon(outputImage));
-
-
-            frame.getContentPane().add(imageLabel, BorderLayout.CENTER);
-            frame.pack();
-            frame.setVisible(true);
-            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        }
+        imageLabel = new JLabel();
+        imageLabel.setIcon(new ImageIcon(outputImage));
 
 
-       public static void main(final String[] _args) throws IOException {
+        frame.getContentPane().add(imageLabel, BorderLayout.CENTER);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
 
-           float convMatrix3x3[] = new float[]{
-                   0f,
-                   -10f,
-                   0f,
-                   -10f,
-                   40f,
-                   -10f,
-                   0f,
-                   -20f,
-                   0f,
-           };
 
-        ConvolutionLambda convolution = new ConvolutionLambda(  new File("C:\\Users\\user1\\aparapi\\branches\\lambda\\dsotm.jpg"));
+    public static void main(final String[] _args) throws IOException {
+
+        float convMatrix3x3[] = new float[]{
+                0f,
+                -10f,
+                0f,
+                -10f,
+                40f,
+                -10f,
+                0f,
+                -20f,
+                0f,
+        };
+
+        ConvolutionLambda convolution = new ConvolutionLambda(new File("C:\\Users\\user1\\aparapi\\branches\\lambda\\dsotm.jpg"));
         Device device = Device.hsa();
-        for (int i=0; i<100; i++){
-        for (float f = -50f; f<-10f; f+=.05f){
-            convMatrix3x3[7]=f;
-        convolution.applyConvolution(convMatrix3x3, device);
-        }
-            for (float f = -10f; f>-50f; f-=.05f){
-                convMatrix3x3[7]=f;
+        for (int i = 0; i < 100; i++) {
+            for (float f = -50f; f < -10f; f += .05f) {
+                convMatrix3x3[7] = f;
+                convolution.applyConvolution(convMatrix3x3, device);
+            }
+            for (float f = -10f; f > -50f; f -= .05f) {
+                convMatrix3x3[7] = f;
                 convolution.applyConvolution(convMatrix3x3, device);
             }
         }
