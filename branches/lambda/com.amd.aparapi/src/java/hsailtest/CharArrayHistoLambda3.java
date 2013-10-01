543677c78ab9b0fa599bb76a645b8e7f424bf8dd
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-10-01 18:57:04 +0000

    char histo looking good

--- branches/lambda/com.amd.aparapi/src/java/hsailtest/CharArrayHistoLambda3.java
+++ branches/lambda/com.amd.aparapi/src/java/hsailtest/CharArrayHistoLambda3.java
@@ -2,24 +2,23 @@ package hsailtest;
 
 import com.amd.aparapi.AparapiException;
 import com.amd.aparapi.Device;
-import static com.amd.aparapi.Device.hsaForEach;
 
-import java.io.*;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
-import java.util.List;
 import java.util.function.IntConsumer;
 
+import static com.amd.aparapi.Device.hsaForEach;
 
 
-public class CharArrayLambda {
+public class CharArrayHistoLambda3 {
 
 
-    static void dump(String type, char[][] _strings, boolean[] results) {
+    static void dump(String type, char[][] _strings, int[] _results) {
         System.out.print(type + " ->");
         boolean first = true;
         for (int i = 0; i < _strings.length; i++) {
-            if (results[i]){
+            if (_results[i]>0){
             if (!first) {
                 System.out.print(", ");
             }   else{
@@ -29,6 +28,7 @@ public class CharArrayLambda {
             for (char c:_strings[i]){
                 System.out.print(c);
             }
+            System.out.print("("+_results[i]+")");
             }
 
         }
@@ -42,41 +42,44 @@ public class CharArrayLambda {
         int len = names.length;
         char[] text = TextTools.getLowercaseTextChars(new File("C:\\Users\\user1\\aparapi\\branches\\lambda\\moby.txt"));
         int textLen = text.length;
-        boolean[] results = new boolean[len];
+        int[] results = new int[len];
         IntConsumer ic = gid -> {
-            boolean result = false;
+            int result = 0;
             char[] nameChars = names[gid];
             int nameCharLen = nameChars.length;
 
-            for (int i=0; !result && i<=textLen-nameCharLen-1; i++){
+            for (int i=0;  i<=textLen-nameCharLen-1; i++){
                 if ((i==0 || text[i-1]==' ') && text[i+nameCharLen]==' ') {
-                    result = true; // optimistic!
-                    for (int offset=0; result && offset<nameCharLen; offset++){
-                        result = nameChars[offset] == text[i+offset];
+                    boolean found = true; // optimistic!
+                    for (int offset=0; found && offset<nameCharLen; offset++){
+                        found = nameChars[offset] == text[i+offset];
+                    }
+                    if (found){
+                        result++;
                     }
                 }
 
             }
             results[gid] = result;
         };
-        Arrays.fill(results, false);
+        Arrays.fill(results, 0);
 
         long start = System.currentTimeMillis();
         Device.jtp().forEach(len, ic);
         System.out.println();
         dump("jtp = "+(System.currentTimeMillis()-start), names, results);
 
-        Arrays.fill(results, false);
+        Arrays.fill(results, 0);
         start = System.currentTimeMillis();
         hsaForEach(len, ic);
         System.out.println();
         dump("hsa1= "+(System.currentTimeMillis()-start), names, results);
-        Arrays.fill(results, false);
+        Arrays.fill(results, 0);
         start = System.currentTimeMillis();
         hsaForEach(len, ic);
         System.out.println();
         dump("hsa2= "+(System.currentTimeMillis()-start), names, results);
-        Arrays.fill(results, false);
+        Arrays.fill(results, 0);
         start = System.currentTimeMillis();
         Device.seq().forEach(len, ic);
         System.out.println();
