3d2f0ea041901057b6802c13c5c74d7e5ac9657d
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2013-10-22 22:22:48 +0000

    Somethng weird with dickens. Creates different answers for multi-books

--- branches/lambda/com.amd.aparapi/src/java/hsailtest/GetDickens2.java
+++ branches/lambda/com.amd.aparapi/src/java/hsailtest/GetDickens2.java
@@ -2,30 +2,24 @@ package hsailtest;
 
 import com.amd.aparapi.Device;
 
-import java.io.*;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Stack;
-import java.util.function.IntConsumer;
 import java.util.Comparator;
 
 
-public class GetDickens {
+public class GetDickens2 {
 
    public class Name {
       char[] name;
-      int[] count;
-      Name(char[] _chars, int _books){
+      volatile int count;
+      Name(char[] _chars){
          name = _chars;
-         count = new int[_books];
+         count = 0;
       }
       int getCount(){
-          int tot = 0;
-          for (int c:count){tot+=c;}
-          return(tot);
+         return (count);
       }
    }
    public class Book {
@@ -63,15 +57,14 @@ public class GetDickens {
          System.out.println();
       }
 
-      void checkNames(Device device, Name[] names, int book) {
+      void checkNames(Device device, Name[] names) {
 
          int textLen = text.length;
          device.forEach(names.length, gid -> {
             Name name = names[gid];
             char[] nameChars = name.name;
             int nameCharLen = nameChars.length;
-             name.count[book]=0;
-            int count = 0;
+            int count = name.count;
             for (int i = 0; i < textLen - nameCharLen; i++) {
                      int offset = 0;
                      while (offset<nameCharLen && (nameChars[offset] == text[i + offset])){
@@ -81,41 +74,30 @@ public class GetDickens {
                           count++;
                       }
                   }
-            name.count[book]=count;
-          //  if (book>0){
-           //     name.accumCount = name.count[book-1]+name.count[book];
-           // }
+            name.count=count;
          });
 
       }
    }
-   void dump(String s, char[][] names, boolean[] found){
 
-   }
 
    public static void main(String[] args) throws IOException {
-      new GetDickens().go();
+      new GetDickens2().go();
    }
 
    public void test(String type, Device dev, Book[] library, Name[] names){
 
       long start = System.currentTimeMillis();
-      int[] counts = new int[names.length];
-      int bookIndex =0;
+
       for (Book book : library) {
-         book.checkNames(dev, names, bookIndex++);
+         book.checkNames(dev, names);
       }
-     // for (int i=0; i<names.length; i++){
-       //   if (names[i].count != counts[i]){
-        //      System.out.println("damn!");
-       //   }
-    // }
+
       long end = System.currentTimeMillis();
 
-      Name[] sortedNames = new Name[names.length];
-      for (int i = 0; i<names.length; i++){
-         sortedNames[i] = names[i];
-      }
+      Name[] sortedNames = Arrays.copyOf(names, names.length);
+
+
       Arrays.sort(sortedNames, new Comparator<Name>(){
          @Override public int compare(Name lhs, Name rhs){
 
@@ -136,11 +118,7 @@ public class GetDickens {
             for (char c : sortedNames[i].name) {
                System.out.print(c);
             }
-            System.out.print("("+sortedNames[i].getCount());
-            for (int book =0; book<bookIndex; book++ ){
-                System.out.print(" "+sortedNames[i].count[book]);
-            }
-             System.out.print(")");
+            System.out.print("("+sortedNames[i].getCount()+")");
          }
 
       }
@@ -152,35 +130,55 @@ public class GetDickens {
       //File lambdaDir = new File("/Users/garyfrost/aparapi/aparapi/branches/lambda");
       File lambdaDir = new File("C:\\Users\\user1\\aparapi\\branches\\lambda");
       File booksDir = new File(lambdaDir, "books/dickens");
-      Book[] library = new Book[] {
+      Book[] library1 = new Book[] {
 
-         new Book("A Tail Of Two Cities", new File(booksDir, "ATailOfTwoCities.txt")),
-             new Book("A Christmas Carol", new File(booksDir, "AChristmasCarol.txt")),
-             new Book("Great Expectations", new File(booksDir, "GreatExpectations.txt")),
-            // new Book("David Copperfield", new File(booksDir, "DavidCopperfield.txt")),
-            // new Book("Nicolas Nickleby", new File(booksDir,"NicolasNickleby.txt")),
+         //new Book("A Tail Of Two Cities", new File(booksDir, "ATailOfTwoCities.txt")),
+           //  new Book("A Christmas Carol", new File(booksDir, "AChristmasCarol.txt")),
+           // new Book("Great Expectations", new File(booksDir, "GreatExpectations.txt")),
+
+       //     new Book("David Copperfield", new File(booksDir, "DavidCopperfield.txt")),
+             new Book("Nicolas Nickleby", new File(booksDir,"NicolasNickleby.txt")),
              //new Book("Oliver Twist", new File(booksDir,"OliverTwist.txt")),
-             //  new Book("A Tail Of Two Cities", new URL("http://www.gutenberg.org/cache/epub/1023/pg1023.txt")),
-             //  new Book("A Christmas Carol", new URL("http://www.gutenberg.org/files/24022/24022-0.txt")),
-             // new Book("Great expectations", new URL("http://www.gutenberg.org/cache/epub/1400/pg1400.txt")),
-             // new Book("David Copperfield", new URL("http://www.gutenberg.org/cache/epub/766/pg766.txt")),
-             // new Book("Nicolas Nickleby", new URL("http://www.gutenberg.org/cache/epub/967/pg967.txt")),
-             // new Book("Oliver Twist", new URL("http://www.gutenberg.org/cache/epub/730/pg730.txt"))
+
       };
-      Device.seq().forEach(library.length, i -> library[i].get());
+       Book[] library2 = new Book[] {
 
+               //  new Book("A Tail Of Two Cities", new File(booksDir, "ATailOfTwoCities.txt")),
+               //  new Book("A Christmas Carol", new File(booksDir, "AChristmasCarol.txt")),
+              // new Book("Great Expectations", new File(booksDir, "GreatExpectations.txt")),
 
-     // char[][] nameChars = TextTools.buildLowerCaseDictionaryChars(new File(lambdaDir, "names.txt"));
+               // new Book("David Copperfield", new File(booksDir, "DavidCopperfield.txt")),
+                new Book("Nicolas Nickleby", new File(booksDir,"NicolasNickleby.txt")),
+               //new Book("Oliver Twist", new File(booksDir,"OliverTwist.txt")),
+
+       };
+      Device.seq().forEach(library1.length, i -> library1[i].get());
+       Device.seq().forEach(library2.length, i -> library2[i].get());
+
       char[][] nameChars = TextTools.buildWhiteSpacePaddedDictionaryChars(new File(lambdaDir, "names.txt"));
-      Name[] names = new Name[nameChars.length];
-      Device.seq().forEach(names.length, i -> names[i] = new Name(nameChars[i], library.length));
+      Name[] names1 = new Name[nameChars.length];
+
+       for (int i=0; i<names1.length; i++){  names1[i] = new Name(nameChars[i]);}
 
 
-      test("hsa", Device.hsa(), library, names);
-      Device.seq().forEach(names.length, i -> { for (int b=0; b<library.length;b++) names[i].count[b]=0;});
-      test("hsa", Device.hsa(), library, names);
-       Device.seq().forEach(names.length, i -> { for (int b=0; b<library.length;b++) names[i].count[b]=0;});
-      test("jtp", Device.jtp(), library, names);
+      test("hsa", Device.hsa(), library1, names1);
+       for (int i=0; i<names1.length; i++){  names1[i].count = 0;}
+      test("hsa", Device.hsa(), library1, names1);
+       for (int i=0; i<names1.length; i++){  names1[i].count = 0;}
+      test("jtp", Device.jtp(), library1, names1);
+       for (int i=0; i<names1.length; i++){  names1[i].count = 0;}
+
+       Name[] names2 = new Name[nameChars.length];
+
+       for (int i=0; i<names2.length; i++){  names2[i] = new Name(nameChars[i]);}
+
+       test("hsa", Device.hsa(), library2, names2);
+       for (int i=0; i<names2.length; i++){  names2[i].count = 0;}
+
+       test("hsa", Device.hsa(), library2, names2);
+       for (int i=0; i<names2.length; i++){  names2[i].count = 0;}
+       test("jtp", Device.jtp(), library2, names2);
+       for (int i=0; i<names2.length; i++){  names2[i].count = 0;}
     //   Device.seq().forEach(names.length, i -> {names[i].accumCount = 0; for (int b=0; b<library.length;b++) names[i].count[b]=0;});
     //  test("seq", Device.seq(), library, names);
 
