87a42f07667ae01683eece418e8b80561865e27a
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-01-29 19:54:09 +0000

    add junit

---
+++ branches/lambda/test/hsailtest/src/hsailtest/SquaresFuncLambdaTest.java
@@ -0,0 +1,58 @@
+package hsailtest;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+import com.amd.aparapi.Device;
+import com.amd.aparapi.HSADevice;
+
+import java.util.function.IntConsumer;
+
+/**
+ * Created by user1 on 1/29/14.
+ */
+public class SquaresFuncLambdaTest {
+
+    static void dump(String type, int[] in, int[] out) {
+        System.out.print(type + " ->");
+        for (int i = 0; i < in.length; i++) {
+            System.out.print("(" + in[i] + "," + out[i] + "),");
+        }
+        System.out.println();
+    }
+
+    static int mul(int lhs, int rhs){
+
+        return(lhs*rhs);
+    }
+
+    static int square(int v){
+        return(mul(v,v));
+    }
+    @Test public void testMain() throws Exception {
+        final int len = 10;
+        int in[] = new int[len];
+        int out[] = new int[len];
+        for (int i=0; i<len; i++){
+            out[i]=0;
+            in[i]=i;
+        }
+        IntConsumer ic = gid -> {
+            out[gid] = square(in[gid]);
+        };
+        ((HSADevice) Device.hsa()).dump(ic);
+
+        if (true){
+            Device.hsa().forEach(len, ic);
+            dump("hsa", in, out);
+            Device.jtp().forEach(len, ic);
+            dump("jtp", in, out);
+            Device.seq().forEach(len, ic);
+            dump("seq", in, out);
+        }
+        assertTrue("Range > max work size", true);
+
+    }
+
+
+    
+}
