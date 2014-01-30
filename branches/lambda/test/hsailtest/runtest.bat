f11d517b6f797e7469f77d665bca0ead9642b95a
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-01-30 17:29:33 +0000

    fixed wierdness with dll paths for unit tests

--- branches/lambda/test/hsailtest/runtest.bat
+++ branches/lambda/test/hsailtest/runtest.bat
@@ -2,9 +2,9 @@ setlocal
 set HSA_RUNTIME=1
 set GPU_BLIT_ENGINE_TYPE=2
 set ENABLE64=1
+set ANT_HOME=C:\Users\user1\apache-ant-1.9.2
 set APARAPI_HOME=C:\Users\user1\aparapi\branches\lambda
 set OKRA_HOME=C:\Users\user1\okra
-set PATH=%OKRA_HOME%\hsa\bin\x86_64;c:\Program Files\Java\jdk1.8.0\bin;%PATH%
-set ANT_OPTS=-agentpath:../../com.amd.aparapi.jni/dist/aparapi_x86_64.dll
+set PATH=%OKRA_HOME%\hsa\bin\x86_64;c:\Program Files\Java\jdk1.8.0\bin;%ANT_HOME%\bin
 ant junit
 endlocal
