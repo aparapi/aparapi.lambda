ae10f47c42ad360c9f9dfc3705acb37a1663664b
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-01-31 16:59:13 +0000

    cleaning test files

--- branches/lambda/test/hsailtest/runtest.bat
+++ branches/lambda/test/hsailtest/runtest.bat
@@ -1,10 +1,6 @@
 setlocal
-set HSA_RUNTIME=1
-set GPU_BLIT_ENGINE_TYPE=2
-set ENABLE64=1
+call ../../env.bat
 set ANT_HOME=C:\Users\user1\apache-ant-1.9.2
-set APARAPI_HOME=C:\Users\user1\aparapi\branches\lambda
-set OKRA_HOME=C:\Users\user1\okra
 set PATH=%OKRA_HOME%\hsa\bin\x86_64;c:\Program Files\Java\jdk1.8.0\bin;%ANT_HOME%\bin
 ant junit
 endlocal
