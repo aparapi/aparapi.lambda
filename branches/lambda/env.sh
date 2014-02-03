ecd60e4766598330c4708f427a9d0bf40eff71f3
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-02-03 16:28:11 +0000

    adding centralized env setup

---
+++ branches/lambda/env.sh
@@ -0,0 +1,10 @@
+export HSA_RUNTIME=1
+export GPU_BLIT_ENGINE_TYPE=2
+export ENABLE64=1
+export APARAPI_HOME=<your path here>
+export JAVA_HOME=<your path here>
+export OKRA_HOME=<yor path here>
+export PATH=${PATH}:${OKRA_HOME}/bin/x86_64
+export JVM_OPTS=-Ddispatch=true -XX:-UseCompressedOops -agentpath:${APARAPI_HOME}/com.amd.aparapi.jni/dist/libaparapi_x86_64.so -Djava.library.path=${OKRA_HOME}/dist/bin:${OKRA_HOME}/hsa/bin/x86_64 
+export JARS=${APARAPI_HOME}/com.amd.aparapi/dist/aparapi.jar:${OKRA_HOME}/dist/okra.jar
+export JAVA="${JAVA_HOME}/bin/java" ${JVM_OPTS}
