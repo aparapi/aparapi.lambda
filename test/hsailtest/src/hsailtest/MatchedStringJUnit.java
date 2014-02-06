85a1a14864175dbe77636f7b04fb4d27aa906c2e
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-02-06 17:32:18 +0000

    added matched string example

---
+++ test/hsailtest/src/hsailtest/MatchedStringJUnit.java
@@ -0,0 +1,70 @@
+package hsailtest;
+
+import com.amd.aparapi.AparapiException;
+import com.amd.aparapi.Device;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.function.IntConsumer;
+
+import static junit.framework.Assert.fail;
+
+
+public class MatchedStringJUnit {
+
+    static class MatchableString{
+        String value;
+        boolean matched;
+        MatchableString(String _value){
+           value = _value;
+        }
+        void containsCheck(String text){
+
+            matched = text.contains(value);
+        }
+
+    }
+
+
+    static void dump(String type, MatchableString[] _strings) {
+        System.out.print(type + " ->");
+        for (int i = 0; i < _strings.length; i++) {
+            if (i != 0) {
+                System.out.print(", ");
+            }
+            System.out.print(_strings[i].value+((_strings[i].matched)?"*":"?"));
+        }
+        System.out.println();
+    }
+
+
+    @Test
+    public void test() {
+        String[] dictionary = new String[]{"cat", "mat", "dog", "car", "ant", "pet", "floor", "man", "table", "boy", "girl", "fork"};
+        MatchableString[] hsaMatchableStrings = new MatchableString[dictionary.length];
+        MatchableString[] matchableStrings = new MatchableString[dictionary.length];
+
+        for (int i=0; i< dictionary.length;i++){
+            hsaMatchableStrings[i]=new MatchableString(dictionary[i]);
+            matchableStrings[i]=new MatchableString(dictionary[i]);
+        }
+
+        int len = dictionary.length;
+        String text = "the cat sat on the mat";
+
+        Device.hsa().forEach(len, gid -> {
+            hsaMatchableStrings[gid].containsCheck(text);
+        });
+        dump("hsa", hsaMatchableStrings);
+        Device.jtp().forEach(len, gid -> {
+            matchableStrings[gid].containsCheck(text);
+        });
+        dump("jtp", hsaMatchableStrings);
+        for (int i=0; i< dictionary.length;i++){
+            if (matchableStrings[i].matched != hsaMatchableStrings[i].matched){
+                fail("at index "+i);
+            }
+        }
+
+    }
+}
