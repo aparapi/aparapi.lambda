6f959b5fd03183f9b173cfd343ef893b2474aeff
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-02-11 19:29:06 +0000

    moving to simple env shared between dev and dist

--- env.bat
+++ env.bat
@@ -6,6 +6,12 @@ set APARAPI_HOME=C:\Users\user1\aparapi\branches\lambda
 set ANT_HOME=C:\Users\user1\apache-ant-1.9.2
 set JAVA_HOME=C:\Program Files\Java\jdk1.8.0
 set OKRA_HOME=C:\Users\user1\okra
+if exist %APARAPI_HOME%\aparapi.jar set APARAPI_JNI_HOME=%APARAPI_HOME%
+if exist %APARAPI_HOME%\aparapi.jar set APARAPI_JAR_HOME=%APARAPI_HOME%
+if not exist %APARAPI_HOME%\aparapi.jar set APARAPI_JNI_HOME=%APARAPI_HOME%\com.amd.aparapi.jni\dist
+if not exist %APARAPI_HOME%\aparapi.jar set APARAPI_JAR_HOME=%APARAPI_HOME%\com.amd.aparapi\dist
+echo APARAPI_JNI_HOME=%APARAPI_JNI_HOME%
+echo APARAPI_JAR_HOME=%APARAPI_JAR_HOME%
 set PATH=
 set PATH=%PATH%;%JAVA_HOME%\bin
 set PATH=%PATH%;%AMD_APP_HOME%\bin\x86_64
@@ -15,7 +21,7 @@ echo PATH=%PATH%
 set JVM_OPTS=
 set JVM_OPTS=%JVM_OPTS% -Ddispatch=true
 set JVM_OPTS=%JVM_OPTS% -XX:-UseCompressedOops
-set JVM_OPTS=%JVM_OPTS% -agentpath:%APARAPI_HOME%\com.amd.aparapi.jni\dist\aparapi_x86_64.dll
+set JVM_OPTS=%JVM_OPTS% -agentpath:%APARAPI_JNI_HOME%\aparapi_x86_64.dll
 set JVM_OPTS=%JVM_OPTS% -Djava.library.path=%OKRA_HOME%\dist\bin;%OKRA_HOME%\hsa\bin\x86_64 
-set JARS=%APARAPI_HOME%\com.amd.aparapi\dist\aparapi.jar;%OKRA_HOME%\dist\okra.jar
+set JARS=%APARAPI_JAR_HOME%\aparapi.jar;%OKRA_HOME%\dist\okra.jar
 
