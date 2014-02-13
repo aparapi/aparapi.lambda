155d40540a1f87fd71e6aacaf351b4f30c34971f
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-02-13 23:32:50 +0000

    removed JAVA var use

--- test/hsailtest/junits.bat
+++ test/hsailtest/junits.bat
@@ -36,8 +36,11 @@ set JARS=%JARS%;../../samples/common/common.jar
 set JARS=%JARS%;hsailtest.jar
 set JARS=%JARS%;.libs/junit-4.10.jar
 
+set JVM_OPTS=%JVM_OPTS% -DshowUI=false
+set JVM_OPTS=%JVM_OPTS% -Dcom.amd.aparapi.enableShowGeneratedHSAIL=false
+
 set CLASS=org.junit.runner.JUnitCore 
 
-%JAVA% -DshowUI=false -Dcom.amd.aparapi.enableShowGeneratedHSAIL=false -classpath %JARS% %CLASS% %TESTS%
+java %JVM_OPTS% -classpath %JARS% %CLASS% %TESTS%
 endlocal
 
