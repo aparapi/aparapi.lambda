0ec2e1698ea43c772d3a73675d97563905d4ee19
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-02-13 23:32:50 +0000

    removed JAVA var use

--- branches/lambda/test/hsailtest/hsailtest.bat
+++ branches/lambda/test/hsailtest/hsailtest.bat
@@ -5,14 +5,12 @@ set JARS=%JARS%;../../samples/common/common.jar
 set JARS=%JARS%;hsailtest.jar
 set JARS=%JARS%;.libs/junit-4.10.jar
 
+set JVM_OPTS=%JVM_OPTS% -Dcom.amd.aparapi.logLevel=OFF
+set JVM_OPTS=%JVM_OPTS% -Dcom.amd.aparapi.enableVerboseJNI=false 
+set JVM_OPTS=%JVM_OPTS% -Dcom.amd.aparapi.dumpFlags=true 
+set JVM_OPTS=%JVM_OPTS% -Dcom.amd.aparapi.enableShowGeneratedHSAIL=false
 
-%JAVA% ^
- -Dcom.amd.aparapi.logLevel=OFF ^
- -Dcom.amd.aparapi.enableVerboseJNI=false ^
- -Dcom.amd.aparapi.dumpFlags=true ^
- -Dcom.amd.aparapi.enableShowGeneratedHSAIL=false ^
- -classpath %JARS% ^
- hsailtest.%1 %2 %3 %4 %5 %6 %7 %8 %9
+java -classpath %JARS%  hsailtest.%1 %2 %3 %4 %5 %6 %7 %8 %9
 endlocal
 
 
--- branches/lambda/test/hsailtest/junit.bat
+++ branches/lambda/test/hsailtest/junit.bat
@@ -3,9 +3,11 @@ call ..\..\env.bat
 set JARS=%JARS%;../../samples/common/common.jar
 set JARS=%JARS%;hsailtest.jar
 set JARS=%JARS%;.libs/junit-4.10.jar
+set JVM_OPTS=%JVM_OPTS% -DshowUI=false
+set JVM_OPTS=%JVM_OPTS% -Dcom.amd.aparapi.enableShowGeneratedHSAIL=false
 
 set CLASS=org.junit.runner.JUnitCore 
 
-%JAVA% -DshowUI=false -Dcom.amd.aparapi.enableShowGeneratedHSAIL=true -classpath %JARS% %CLASS% hsailtest.%1%JUnit
+java %JVM_OPTS% -classpath %JARS% %CLASS% hsailtest.%1%JUnit
 endlocal
 
