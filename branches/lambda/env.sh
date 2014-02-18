3e225c4382fe93865d6b9567f445fc79c26647f1
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-02-18 18:29:50 +0000

    bringup on linux

--- branches/lambda/env.sh
+++ branches/lambda/env.sh
@@ -1,46 +1,72 @@
-export HSA_RUNTIME=1
-echo "HSA_RUNTIME:$HSA_RUNTIME"
+###################################
+# If you create myEnv.sh in this dir it can overide any of these. 
 
-export ANT_HOME=/Users/garyfrost/apache-ant-1.9.2
+# set APARAPI_HOME to the current directory
+export APARAPI_HOME=/home/${LOGNAME}/aparapi-lambda
+
+# set ANT_HOME to the  directory where ant is installed (/bin is beneath here)
 export ANT_HOME=/usr/share/ant
-echo "ANT_HOME:${ANT_HOME}"
 
-export APARAPI_HOME=/Users/garyfrost/aparapi/aparapi/branches/lambda
-export APARAPI_HOME=/home/user1/aparapi-lambda
+# set JAVA_HOME to a java 8 sdk install (/bin and /jre are beneath here)
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+
+# set OCL_LIB and OCL_INCLUDE to point to your OpenCL SDK installation lib and include dirs (so Aparapi com.amd.aparapi.jni can build)
+export OCL_HOME=${APARAPI_HOME}/../ocl
+export OCL_LIB=${OCL_HOME}/lib/x86_64
+export OCL_INCLUDE=${OCL_HOME}/include
+
+# set OKRA_HOME to your OKRA directory (/dist is underneath here)
+export OKRA_HOME=${APARAPI_HOME}/../okra
+
+if test -f ${APARAPI_HOME}/myEnv.sh ; then 
+   echo "${APARAPI_HOME}/myEnv.sh content start #########{"
+   . ${APARAPI_HOME}/myEnv.sh 
+   echo "} ############ ${APARAPI_HOME}/myEnv.sh content end"
+fi
+
+###################################
 echo "APARAPI_HOME:${APARAPI_HOME}"
+echo "ANT_HOME:${ANT_HOME}"
+echo "APARAPI_JNI_HOME:${APARAPI_JNI_HOME}"
+echo "APARAPI_JAR_HOME:${APARAPI_JAR_HOME}"
+echo "JAVA_HOME:${JAVA_HOME}"
+echo "OCL_LIB:${OCL_LIB}"
+echo "OCL_INCLUDE:${OCL_INCLUDE}"
+echo "OKRA_HOME:${OKRA_HOME}"
 
+# This looks odd, but when we create a dist zip the dist has aparapi.jar and {lib}aparapi_x86_64.{dll|dylib|so} at the root
+# The following allows this env script to work for either configuration.
 test -d ${APARAPI_HOME}/com.amd.aparapi.jni/dist && export APARAPI_JNI_HOME=${APARAPI_HOME}/com.amd.aparapi.jni/dist  || export APARAPI_JNI_HOME=${APARAPI_HOME}
 test -d ${APARAPI_HOME}/com.amd.aparapi/dist && export APARAPI_JAR_HOME=${APARAPI_HOME}/com.amd.aparapi/dist  || export APARAPI_JAR_HOME=${APARAPI_HOME}
 
-echo "APARAPI_JNI_HOME:${APARAPI_JNI_HOME}"
-echo "APARAPI_JAR_HOME:${APARAPI_JAR_HOME}"
-
-export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0.jdk/Contents/Home/
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-echo "JAVA_HOME:${JAVA_HOME}"
-
-export OKRA_HOME=/home/user1/SumatraDemos/okra
-echo "OKRA_HOME:${OKRA_HOME}"
 
 export PATH=${JAVA_HOME}/bin:${ANT_HOME}/bin:${OKRA_HOME}/dist/bin:${OKRA_HOME}/hsa/bin/x86_64:${PATH}
 echo "PATH:$PATH"
 
-export LD_LIBRARY_PATH=${OKRA_HOME}/dist/bin:${OKRA_HOME}/hsa/bin/x86_64:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=${OKRA_HOME}/dist/bin:${OKRA_HOME}/hsa/bin/x86_64:${OCL_LIB}:${LD_LIBRARY_PATH}
 echo "LD_LIBRARY_PATH:${LD_LIBRARY_PATH}"
 
-export LIBOS=libaparapi_$(uname -m)
+export LIB_ARCH=$(uname -m)
+echo "LIB_ARCH:${LIB_ARCH}"
 case $(uname -s) in 
-  Darwin) LIBNAME=${LIBOS}.dyLib;;
-  Linux)  LIBNAME=${LIBOS}.so;;
+  Darwin) LIB_PREFIX=lib;;
+  Linux)  LIB_PREFIX=lib;;
 esac
-echo libname = ${LIBNAME}
+case $(uname -s) in 
+  Darwin) LIB_SUFFIX=dyLib;;
+  Linux)  LIB_SUFFIX=so;;
+esac
+echo "LIB_SUFFIX:${LIB_SUFFIX}"
+echo "LIB_PREFIX:${LIB_PREFIX}"
 
+export HSA_RUNTIME=1
+echo "HSA_RUNTIME:${HSA_RUNTIME}"
 
 export JVM_OPTS=
 export JVM_OPTS="${JVM_OPTS} -Xmx2G"
 export JVM_OPTS="${JVM_OPTS} -Ddispatch=true"
 export JVM_OPTS="${JVM_OPTS} -XX:-UseCompressedOops"
-export JVM_OPTS="${JVM_OPTS} -agentpath:${APARAPI_JNI_HOME}/${LIBNAME}"
+export JVM_OPTS="${JVM_OPTS} -agentpath:${APARAPI_JNI_HOME}/${LIB_PREFIX}aparapi_${LIB_ARCH}.${LIB_SUFFIX}"
 export JVM_OPTS="${JVM_OPTS} -Djava.library.path=${OKRA_HOME}/dist/bin:${OKRA_HOME}/hsa/bin/x86_64"
 echo "JVM_OPTS:$JVM_OPTS"
 
--- branches/lambda/samples/mandel/hsailmandel.sh
+++ branches/lambda/samples/mandel/hsailmandel.sh
@@ -6,5 +6,6 @@ export JARS="${JARS}:../common/common.jar"
 export JVM_OPTS="${JVM_OPTS} -Dcom.amd.aparapi.useAgent=true"
 export JVM_OPTS="${JVM_OPTS} -Dcom.amd.aparapi.executionMode=${1}"
 export JVM_OPTS="${JVM_OPTS} -Dcom.amd.aparapi.enableVerboseJNI=false"
+export JVM_OPTS="${JVM_OPTS} -Dcom.amd.aparapi.enableShowGeneratedHSAIL=true"
 
 java ${JVM_OPTS} -classpath ${JARS} com.amd.aparapi.sample.mandel.HSAILMandel
