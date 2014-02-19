84d02ac82d429f2de2b73f7fbe3bf415bd9ed375
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-02-19 17:33:36 +0000

    fixed coherence switch

--- test/hsailtest/hsailtest.sh
+++ test/hsailtest/hsailtest.sh
@@ -1,4 +1,5 @@
 . ../../env.sh 
+export OKRA_HSA_NON_COHERENCE=1
 export JARS="${JARS} ../../samples/common/common.jar"
 export JARS="${JARS} hsailtest.jar"
 
--- test/hsailtest/junit.sh
+++ test/hsailtest/junit.sh
@@ -1,10 +1,12 @@
 . ../../env.sh
+#export OKRA_HSA_NON_COHERENT=1
 export JARS="${JARS}:../../samples/common/common.jar"
 export JARS="${JARS}:hsailtest.jar"
 export JARS="${JARS}:.libs/junit-4.10.jar"
 
 export JVM_OPTS="${JVM_OPTS} -DshowUI=false"
 export JVM_OPTS="${JVM_OPTS} -Dcom.amd.aparapi.enableShowGeneratedHSAIL=false"
+export JVM_OPTS="${JVM_OPTS} -Dcom.amd.aparapi.enableSetOKRACoherence=true"
 
 export CLASS=org.junit.runner.JUnitCore
  
