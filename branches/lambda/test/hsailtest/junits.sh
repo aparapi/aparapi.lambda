67eccacb8a1bab69387f3a22735ab11536197cf2
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-02-20 22:35:13 +0000

    arranged for junits.sh to list failures

--- branches/lambda/test/hsailtest/junits.sh
+++ branches/lambda/test/hsailtest/junits.sh
@@ -1,36 +1,36 @@
 . ../../env.sh
 
 export TESTS=" \
-    hsailtest.FloatSquaresFuncJUnit \
-    hsailtest.IntSquaresFuncJUnit \
-    hsailtest.IntMaxJUnit \
-    hsailtest.IntMinJUnit \
-    hsailtest.SqrtJUnit \
-    hsailtest.OopArray2DJUnit \
-    hsailtest.InitBodyJUnit \
-    hsailtest.FieldIntSquaresJUnit \
-    hsailtest.IntFieldAssignJUnit \
-    hsailtest.DoubleSquaresJUnit \
-    hsailtest.OddEvenJUnit \
-    hsailtest.OddEvenFuncJUnit \
-    hsailtest.TernaryOddEvenJUnit \
-    hsailtest.StringContainsJUnit \
-    hsailtest.StringCharAtJUnit \
-    hsailtest.IntArray2DJUnit \
-    hsailtest.StringHashCodeJUnit \
-    hsailtest.StringLenJUnit \
-    hsailtest.StringIndexOfJUnit \
-    hsailtest.HypotJUnit \
-    hsailtest.MandelJUnit \
-    hsailtest.DickensJUnit \
-    hsailtest.MatchedStringJUnit \
-    hsailtest.CharArrayStateMachineJUnit \
-    hsailtest.CharAddJUnit \
-    hsailtest.SinCosJUnit \
-    hsailtest.IntVectorMultiplyAddJUnit \
-    hsailtest.OopPointsJUnit \
-    hsailtest.OopPointsDefaultFieldsNoAccessorsJUnit \
-    hsailtest.StaticFieldAccess"
+    FloatSquaresFunc\
+    IntSquaresFunc\
+    IntMax\
+    IntMin\
+    Sqrt\
+    OopArray2D\
+    InitBody\
+    FieldIntSquares\
+    IntFieldAssign\
+    DoubleSquares\
+    OddEven\
+    OddEvenFunc\
+    TernaryOddEven\
+    StringContains\
+    StringCharAt\
+    IntArray2D\
+    StringHashCode\
+    StringLen\
+    StringIndexOf\
+    Hypot\
+    Mandel\
+    Dickens\
+    MatchedString\
+    CharArrayStateMachine\
+    CharAdd\
+    SinCos\
+    IntVectorMultiplyAdd\
+    OopPoints\
+    OopPointsDefaultFieldsNoAccessors\
+    StaticFieldAccess"
 
 export JARS="${JARS}:../../samples/common/common.jar"
 export JARS="${JARS}:hsailtest.jar"
@@ -39,6 +39,6 @@ export JARS="${JARS}:.libs/junit-4.10.jar"
 export JVM_OPTS="${JVM_OPTS} -DshowUI=false"
 export JVM_OPTS="${JVM_OPTS} -Dcom.amd.aparapi.enableShowGeneratedHSAIL=false"
 
-export CLASS=org.junit.runner.JUnitCore 
+export CLASS=hsailtest.JUnitTester
  
 java ${JVM_OPTS} -classpath ${JARS} ${CLASS} ${TESTS}
---
+++ branches/lambda/test/hsailtest/src/hsailtest/JUnitTester.java
@@ -0,0 +1,34 @@
+package hsailtest;
+
+import java.util.List;
+import java.util.ArrayList;
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class JUnitTester{
+   public static void  main(String[] args){
+      List<Class<?>> classList = new ArrayList<Class<?>>();
+      for (String arg: args){
+          Class<?> clazz = null;
+          try{
+             clazz=  Class.forName("hsailtest."+arg+"JUnit");
+             classList.add(clazz);
+          }catch(ClassNotFoundException cnf){
+             System.out.println("skipping "+arg);
+          }
+      }
+      Class<?>[] classes = classList.toArray(new Class<?>[0]);
+      JUnitCore tester = new JUnitCore();
+      Result result = tester.run(classes);
+      System.out.println("--------------------------------");
+      System.out.println("Ran "+result.getRunCount()+" tests");
+      if (result.getFailures().size()>1){
+      System.out.println(result.getFailures().size()+ " FAILURES follow");
+      for (Failure f: result.getFailures()){
+         System.out.println("failure ="+f.getDescription()+" "+f.getMessage());
+      }
+      }
+   }
+
+}
