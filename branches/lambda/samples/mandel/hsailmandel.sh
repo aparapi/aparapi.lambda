8a34f42e8594c77bc6934a0766c5c3e5cdb1c907
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-02-20 19:12:22 +0000

    allow user to pass coherence flag

--- branches/lambda/samples/mandel/hsailmandel.sh
+++ branches/lambda/samples/mandel/hsailmandel.sh
@@ -3,9 +3,8 @@
 export JARS="${JARS}:mandel.jar"
 export JARS="${JARS}:../common/common.jar"
 
-export JVM_OPTS="${JVM_OPTS} -Dcom.amd.aparapi.useAgent=true"
-export JVM_OPTS="${JVM_OPTS} -Dcom.amd.aparapi.executionMode=${1}"
 export JVM_OPTS="${JVM_OPTS} -Dcom.amd.aparapi.enableVerboseJNI=false"
 export JVM_OPTS="${JVM_OPTS} -Dcom.amd.aparapi.enableShowGeneratedHSAIL=true"
+export JVM_OPTS="${JVM_OPTS} -Dcom.amd.aparapi.enableSetOKRACoherence=$1"
 
 java ${JVM_OPTS} -classpath ${JARS} com.amd.aparapi.sample.mandel.HSAILMandel
