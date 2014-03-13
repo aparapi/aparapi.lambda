a672d9a9ea68742cfb89d70b187eb6b5914b321f
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-03-13 22:46:43 +0000

    some wishlists in HSALab

--- branches/lambda/com.amd.aparapi/src/java/com/amd/aparapi/Device.java
+++ branches/lambda/com.amd.aparapi/src/java/com/amd/aparapi/Device.java
@@ -3,12 +3,21 @@ package com.amd.aparapi;
 import com.amd.aparapi.OpenCLDevice.DeviceComparitor;
 import com.amd.aparapi.OpenCLDevice.DeviceSelector;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.function.IntConsumer;
 
 public abstract class Device<T extends Device>{
 
+    public interface IntMapper{ int map(int _in);}
 
-
+    int sum(int from, int to, IntMapper im){
+        int sum = 0;
+        for (int i=from; i<to; i++){
+            sum+=im.map(i);
+        }
+        return(sum);
+    }
     static public class IntRange{
        int from;
        int to;
@@ -23,6 +32,16 @@ public abstract class Device<T extends Device>{
 
            Device.seq().forEach(from, to, _ic);
        }
+       public int sum(IntMapper _im){
+           return(Device.seq().sum(from, to, _im));
+       }
+        public int mapReduce(IntMapper _im, IntReducer _ir){
+            int result=0;
+            for (int i=from; i<to;  i++){
+                result=_ir.reduce(_im.map(i), result);
+            }
+            return(result);
+        }
     }
 
     static public class ParallelIntRange {
@@ -51,23 +70,66 @@ public abstract class Device<T extends Device>{
        }
     }
 
+    public interface Mapper<T>{ int map(T t);}
+    public interface IntReducer<T>{ int reduce(int lhs, int rhs);}
     static public class ArrayRange<T>{
        T[] arr;
+       int usableLength;
        ArrayRange(T[] _arr){
           arr = _arr;
+           usableLength=_arr.length;
        }
+        ArrayRange(T[] _arr, int _usableLength){
+            arr = _arr;
+            usableLength=_usableLength;
+        }
        public ParallelArrayRange<T> parallel(){
           return(new ParallelArrayRange(this));
        }
        public void forEach(ObjectConsumer<T> _oc){
-          for (T i :arr){
-             _oc.accept(i);
+          for (int i=0; i<usableLength; i++){
+             _oc.accept(arr[i]);
           }
        }
+        public int sum(Mapper<T> _im){
+            int sum = 0;
+            for (int i=0; i<usableLength; i++){
+                sum+=_im.map(arr[i]);
+            }
+            return(sum);
+        }
+        public int mapReduce(Mapper<T> _im, IntReducer _ir){
+            int result=0;
+            for (int i=0; i<usableLength; i++){
+                result=_ir.reduce(_im.map(arr[i]), result);
+            }
+            return(result);
+        }
     }
 
-    static public <T> ArrayRange<T> range(T[] _arr){
-       return((ArrayRange<T>)new ArrayRange(_arr));
+
+    static public <T> ArrayRange<T> range(ArrayList<T> _list)  {
+        T[] arr;
+        try{
+        Field f = ArrayList.class.getDeclaredField("elementData");
+            f.setAccessible(true);
+        arr = (T[])f.get(_list);
+
+        return((ArrayRange<T>)new ArrayRange(arr, _list.size()));
+    } catch (NoSuchFieldException e) {
+            e.printStackTrace();
+            return (null);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            return (null);
+        }
+    }
+
+    static public <T> ArrayRange<T> range(T[] _arr)  {
+
+
+            return((ArrayRange<T>)new ArrayRange(_arr));
+
     }
 
     static public IntRange range(int _from, int _to){
@@ -77,10 +139,7 @@ public abstract class Device<T extends Device>{
         return(new IntRange(0, _to));
     }
 
-    static void foo(){
-       range(new String[0]);
-    }
-   
+
     public static Device getByName(String _deviceName) {
         TYPE type = TYPE.valueOf(_deviceName.toUpperCase());
         return(_deviceName.equals("hsa")?Device.hsa():
