a8be255fa6cd6d190cf0ab727a4086fb857b94fc
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-03-13 19:08:14 +0000

    env.sh edits

--- env.sh
+++ env.sh
@@ -1,30 +1,48 @@
-###################################
-# If you create myEnv.sh in this dir it can overide any of these. 
+export TOP=/home/${LOGNAME}
+# Rest of this assumes layout below
+#   ${TOP}
+#       +---aparapi-lambda  (this dir!)
+#       +---okra            (for HSA rt)
+#       +---ocl             (for opencl headers and libs)
+#       +---jdk1.8.0        Java 8 JDK
 
-# set APARAPI_HOME to the current directory
-export APARAPI_HOME=/home/${LOGNAME}/aparapi-lambda
-
-# set ANT_HOME to the  directory where ant is installed (/bin is beneath here)
-export ANT_HOME=/usr/share/ant
-
-# set JAVA_HOME to a java 8 sdk install (/bin and /jre are beneath here)
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-
-# set OCL_LIB and OCL_INCLUDE to point to your OpenCL SDK installation lib and include dirs (so Aparapi com.amd.aparapi.jni can build)
-export OCL_HOME=/home/${LOGNAME}/ocl
-export OCL_LIB=${OCL_HOME}/lib/x86_64
-export OCL_INCLUDE=${OCL_HOME}/include
-
-# set OKRA_HOME to your OKRA directory (/dist is underneath here)
-export OKRA_HOME=/home/${LOGNAME}/okra
-
-if test -f ${APARAPI_HOME}/myEnv.sh ; then 
-   echo "${APARAPI_HOME}/myEnv.sh content start #########{"
-   . ${APARAPI_HOME}/myEnv.sh 
-   echo "} ############ ${APARAPI_HOME}/myEnv.sh content end"
+# set APARAPI_HOME to the directory containing this file
+export APARAPI_HOME=${TOP}/aparapi-lambda
+if test ! -f ${APARAPI_HOME}/env.sh ; then 
+   echo "APARAPI_HOME is not set correctly (currently set to ${APARAPI_HOME})"
+   exit 1
 fi
 
-###################################
+# set JAVA_HOME to a java 8 sdk install (/bin and /jre are beneath here)
+export JAVA_HOME=${TOP}/jdk1.8.0
+if test ! -f ${JAVA_HOME}/bin/javac ; then 
+   echo "JAVA_HOME is not set correctly (currently set to ${JAVA_HOME})"
+   exit 1
+fi
+
+# set ANT_HOME to the directory where ant is installed (/bin is beneath here)
+export ANT_HOME=/usr/share/ant
+if test ! -f ${ANT_HOME}/bin/ant ; then 
+   echo "ANT_HOME is not set correctly (currently set to ${ANT_HOME})"
+   exit 1
+fi
+
+# OCL_LIB + OCL_INCLUDE point to OpenCL SDK installation lib and include dirs (so Aparapi com.amd.aparapi.jni can build)
+export OCL_HOME=${TOP}/ocl
+export OCL_LIB=${OCL_HOME}/lib/x86_64
+export OCL_INCLUDE=${OCL_HOME}/include
+if test ! -d ${OCL_HOME}/include ; then 
+   echo "OCL_HOME is not set correctly (currently set to ${OCL_HOME})"
+   exit 1
+fi
+
+# set OKRA_HOME to your OKRA directory (/dist is underneath here)
+export OKRA_HOME=${TOP}/okra
+if test ! -d ${OKRA_HOME}/dist ; then 
+   echo "OKRA_HOME is not set correctly (currently set to ${OKRA_HOME})"
+   exit 1
+fi
+
 echo "APARAPI_HOME:${APARAPI_HOME}"
 echo "ANT_HOME:${ANT_HOME}"
 echo "APARAPI_JNI_HOME:${APARAPI_JNI_HOME}"
@@ -34,11 +52,10 @@ echo "OCL_LIB:${OCL_LIB}"
 echo "OCL_INCLUDE:${OCL_INCLUDE}"
 echo "OKRA_HOME:${OKRA_HOME}"
 
-# This looks odd, but when we create a dist zip the dist has aparapi.jar and {lib}aparapi_x86_64.{dll|dylib|so} at the root
+# This looks odd :) When we create dist zip the dist has aparapi.jar and {lib}aparapi_x86_64.{dll|dylib|so} at the root
 # The following allows this env script to work for either configuration.
 test -d ${APARAPI_HOME}/com.amd.aparapi.jni/dist && export APARAPI_JNI_HOME=${APARAPI_HOME}/com.amd.aparapi.jni/dist  || export APARAPI_JNI_HOME=${APARAPI_HOME}
 test -d ${APARAPI_HOME}/com.amd.aparapi/dist && export APARAPI_JAR_HOME=${APARAPI_HOME}/com.amd.aparapi/dist  || export APARAPI_JAR_HOME=${APARAPI_HOME}
-
 
 export PATH=${JAVA_HOME}/bin:${ANT_HOME}/bin:${OKRA_HOME}/dist/bin:${OKRA_HOME}/hsa/bin/x86_64:${PATH}
 echo "PATH:$PATH"
@@ -51,22 +68,26 @@ echo "LIB_ARCH:${LIB_ARCH}"
 case $(uname -s) in 
   Darwin) LIB_PREFIX=lib;;
   Linux)  LIB_PREFIX=lib;;
+  Win64)  LIB_PREFIX=;;
 esac
 case $(uname -s) in 
   Darwin) LIB_SUFFIX=dyLib;;
   Linux)  LIB_SUFFIX=so;;
+  Win64)  LIB_SUFFIX=dll;;
 esac
 echo "LIB_SUFFIX:${LIB_SUFFIX}"
 echo "LIB_PREFIX:${LIB_PREFIX}"
+export APARAPI_AGENT_NAME=${LIB_PREFIX}aparapi_${LIB_ARCH}.${LIB_SUFFIX}
+echo APARAPI_AGENT_NAME=${APARAPI_AGENT_NAME}
+
 
 export HSA_RUNTIME=1
 echo "HSA_RUNTIME:${HSA_RUNTIME}"
 
 export JVM_OPTS=
 export JVM_OPTS="${JVM_OPTS} -Xmx2G"
-export JVM_OPTS="${JVM_OPTS} -Ddispatch=true"
 export JVM_OPTS="${JVM_OPTS} -XX:-UseCompressedOops"
-export JVM_OPTS="${JVM_OPTS} -agentpath:${APARAPI_JNI_HOME}/${LIB_PREFIX}aparapi_${LIB_ARCH}.${LIB_SUFFIX}"
+export JVM_OPTS="${JVM_OPTS} -agentpath:${APARAPI_JNI_HOME}/${APARAPI_AGENT_NAME}"
 export JVM_OPTS="${JVM_OPTS} -Djava.library.path=${OKRA_HOME}/dist/bin:${OKRA_HOME}/hsa/bin/x86_64"
 echo "JVM_OPTS:$JVM_OPTS"
 
