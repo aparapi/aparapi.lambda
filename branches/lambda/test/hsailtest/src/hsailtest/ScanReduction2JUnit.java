137e15a573f373c4d1783896c709a678c12db5d4
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-04-01 23:13:54 +0000

    another scan implementation

--- branches/lambda/test/hsailtest/src/hsailtest/ScanReduction2JUnit.java
+++ branches/lambda/test/hsailtest/src/hsailtest/ScanReduction2JUnit.java
@@ -22,25 +22,22 @@ public class ScanReduction2JUnit {
            int lsize = getCurrentWorkGroupSize();
            local[lid] = arr[id];
            barrier();
-           for (int step = 2; step <= lsize; step *= 2) {
+           for (int step = 2; step <= lsize; step*=2) {
                if ((lid+1)%step == 0) {
-                   int halfStep = step/2;
-                   local[lid] +=  local[lid-halfStep];
+                   int stride = step/2;
+                   local[lid] +=  local[lid-stride];
                }
                barrier();
            }
-           if((lid+1)==getCurrentWorkGroupSize()) {// last in group
-
-               partials[getWorkGroupId()] = local[lid]; // race here is ok
-               local[lid]=0;
+           if (lid == lsize-1){
+               partials[getWorkGroupId()]=local[lid]; // extract the partial
            }
+
            barrier();
-           for (int step=lsize; step >1 ; step/=2){
-               if (((lid+1)%step) == 0){
-                   int halfStep = step/2;
-                   int prev = local[lid-halfStep];
-                   local[lid-halfStep]=local[lid];
-                   local[lid]+= prev;
+           for (int step=lsize/2; step>1 ; step/=2){ // 4, 2, 1
+               if (((lid+1)<lsize)&&(((lid+1)%step)==0)){ // lid = {3} {1,3,5}
+                   int stride = step/2; //2, 1
+                   local[lid+stride] = local[lid] + local[lid+stride];
                }
                barrier();
            }
@@ -54,39 +51,7 @@ public class ScanReduction2JUnit {
        }
        return(sum);
    }
-    /*
 
-    __kernel void prefixKernel( __local int* scratch, __global int* data){
-        int gid = get_global_id(0);
-        int lid = get_local_id(0);
-        int lsize = get_local_size(0);
-
-        scratch[lid]= data[gid]; // copy into local scratch for the reduction
-        barrier(CLK_LOCAL_MEM_FENCE); // make sure all of scratch is populated
-
-        for (int step=2; step <=lsize; step<<=1){
-            if (((lid+1)%step) == 0){
-                scratch[lid]+=scratch[lid-(step>>1)];
-            }
-            barrier(CLK_LOCAL_MEM_FENCE);
-        }
-
-        if ((lid+1) == get_local_size(0)){
-            scratch[lid]=0;
-        }
-
-        barrier(CLK_LOCAL_MEM_FENCE);
-        for (int step=lsize; step >1 ; step>>=1){
-            if (((lid+1)%step) == 0){
-                int prev = scratch[lid-(step>>1)];
-                scratch[lid-(step>>1)]=scratch[lid];
-                scratch[lid]+= prev;
-            }
-            barrier(CLK_LOCAL_MEM_FENCE);
-        }
-        data[gid] = scratch[lid];
-    }
-*/
 
 //  4    2    3    2    6    1    2    3
 //   \   |     \   |     \   |     \   |
@@ -114,7 +79,7 @@ public class ScanReduction2JUnit {
 //                       \             |
 //                        \            |
 //                         \           |
-//                          \          |   This last pass can be ommitted!
+//                          \          |
 //                           \         |
 //                            \        |
 //                             \       |
@@ -127,48 +92,23 @@ public class ScanReduction2JUnit {
 //                                    \|
 //                                     +
 //  4    6    3   11    6    7    2   23
-//                                     |
-//                             overwrite with 0
-//                                     |
-//                                     V
-//  4    6    3   11    6    7    2    0
-//                  \                 /|
-//                   \               / |
-//                    \             /  |
-//                     \           /   |
-//                      \         /    |
-//                       \       /     |
-//                        \     /      |
-//                         \   /       |
-//                          \ /        |
-//                           /         |
-//                          / \        |
-//                         /   \       |
-//                        /     \      |
-//                       /       \     |
-//                      /         \    |
-//                     /           \   |
-//                    /             \  |
-//                   /               \ |
-//                  /                 \|
-//                 V                   +
-//  4    6    3    0    6    7    2    11
-//         \      /|          \      / |
-//          \    / |           \    /  |
-//           \  /  |            \  /   |
-//            \/   |             \/    |
-//            /\   |             /\    |
-//           /  \  |            /  \   |
-//          /    \ |           /    \  |
-//         /      \|          /      \ |
-//        V        +         V         +
-//  4     0   3    6    6   11    2    18
-//    \  /|    \  /|     \  /|     \  /|
-//     \/ |     \/ |      \/ |      \/ |
-//     /\ |     /\ |      /\ |      /\ |
-//    /  \|    /  \|     /  \|     /  \|
-//   V    +   V    +     V   +    V    +
-//  0     4   6    9    11  17    18   20
+//                  \        |
+//                   \       |
+//                    \      |
+//                     \     |
+//                      \    |
+//                       \   |
+//                        \  |
+//                         \ |
+//                          \|
+//                           +
+//  4    6    3   11    6   18    2   23
+//        \   |     \   |     \   |
+//         \  |      \  |      \  |
+//          \ |       \ |       \ |
+//           \|        \|        \|
+//            +         +         +
+//  4    6   10   11   17   18   20   23
 
 
 
@@ -184,9 +124,9 @@ public class ScanReduction2JUnit {
         JunitHelper.dump("orig", inCopy);
        JunitHelper.dump(" hsa", in);
        int[] out = new int[len];
-
+        out[0]=inCopy[0];
         for (int i=1; i<len; i++){
-            out[i] = out[i-1] + inCopy[i-1];
+            out[i] = out[i-1] + inCopy[i];
         }
         int sum=out[len-1]+inCopy[len-1];
 
