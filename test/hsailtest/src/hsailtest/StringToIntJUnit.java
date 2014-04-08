649d9a00ef4ef32aebd39154756e6c01474c3b7a
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-04-08 18:12:38 +0000

    added String to int..  fails

---
+++ test/hsailtest/src/hsailtest/StringToIntJUnit.java
@@ -0,0 +1,36 @@
+package hsailtest;
+
+import com.amd.aparapi.Device;
+import org.junit.Test;
+
+import static com.amd.aparapi.HSA.*;
+import static org.junit.Assert.assertTrue;
+
+
+public class StringToIntJUnit {
+
+
+
+
+   @Test public void test() throws Exception{
+       String[] strings = new String[256];
+       int[] ints = new int[strings.length];
+       Device.jtp().forEach(strings.length, id -> strings[id] = Integer.toString(id));
+       JunitHelper.dump("before", strings);
+
+           Device.hsa().forEach(strings.length, id -> {
+               String[] local = (String[])localObjectX1();
+               int lid = getWorkItemId();
+               local[lid] = strings[id];
+               barrier();
+               ints[(id+1)%getCurrentWorkGroupSize()]=Integer.valueOf(local[lid]);
+           });
+
+
+    JunitHelper.dump("after", ints);
+
+
+
+        assertTrue("HSA equals sequential results", ints[0]==255);
+    }
+}
