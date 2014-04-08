f63f22e848ccf376747c9e90e81a5e9f1c90c6ef
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-04-08 19:35:05 +0000

    added string find test - fails

---
+++ branches/lambda/test/hsailtest/src/hsailtest/StringFindJUnit.java
@@ -0,0 +1,35 @@
+package hsailtest;
+
+import com.amd.aparapi.Device;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.amd.aparapi.HSA.*;
+import static org.junit.Assert.assertTrue;
+
+
+public class StringFindJUnit {
+
+
+
+   @Test public void test() throws Exception{
+       String[] lines = new String[]{"one", "two", "three", "four"};
+       boolean[] flags = new boolean[]{false};
+
+           Device.hsa().forEach(lines, line -> {
+               if (line.length()==3){
+                   flags[0]=true;
+               }
+
+
+           });
+
+        assertTrue("HSA equals sequential results", flags[0]);
+    }
+}
