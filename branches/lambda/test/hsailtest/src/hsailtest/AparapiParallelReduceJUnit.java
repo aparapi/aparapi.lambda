d462adc2c1eda506a5c9f47c9fb6011b7ab3db17
frost.gary@gmail.com <frost.gary@gmail.com@2eaaf48d-f2ec-fad7-13b6-307ea5940990>
2014-04-21 21:09:38 +0000

    adding more reduce options

--- branches/lambda/test/hsailtest/src/hsailtest/AparapiParallelReduceJUnit.java
+++ branches/lambda/test/hsailtest/src/hsailtest/AparapiParallelReduceJUnit.java
@@ -5,12 +5,12 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
-public class AparapiParallelMapReduceJUnit{
+public class AparapiParallelReduceJUnit{
 
    @Test
    public void testMin(){
 
-      int min = Aparapi.range(0, 12).parallel().map(i -> i).reduce((l, r) -> l<r?l:r);
+      int min = Aparapi.range(0, 12).parallel().reduce((l, r) -> l<r?l:r);
       System.out.println("min "+min);
       assertTrue("min==0", min == 0);
 
@@ -19,7 +19,7 @@ public class AparapiParallelMapReduceJUnit{
    @Test
    public void testMax(){
 
-      int max = Aparapi.range(0, 12).parallel().map(i -> i*2).reduce((l, r) -> l>r?l:r);
+      int max = Aparapi.range(0, 12).parallel().reduce((l, r) -> l>r?l:r);
 
       System.out.println("max "+max);
       assertTrue("max==0", max == 22);
@@ -29,7 +29,7 @@ public class AparapiParallelMapReduceJUnit{
    @Test
    public void testSum(){
 
-      int sum = Aparapi.range(0, 12).parallel().map(i -> i).reduce((l, r) -> l+r);
+      int sum = Aparapi.range(0, 12).map(i -> i).reduce((l, r) -> l+r);
 
       System.out.println("sum "+sum);
       assertTrue("sum==0", sum == 66);
